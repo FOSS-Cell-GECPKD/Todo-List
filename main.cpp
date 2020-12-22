486a035dd368f3a74d56824672bd926a98fb9ab5
AmalChandru <amaltathva@gmail.com>
2020-12-22 18:55:07 +0530

    Created help function, fixed typos in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
 #include<iostream>
-using nampespace std;
+using namespace std;
 
+void help(){
+	cout<<"•••••••••••••Welcome to To do List•••••••••••••••\n"
+          ""
+          "1. Add a new to do\n"
+          "2. Show remaining todos\n"
+          "3. Delete a todo\n"
+          "4. Complete a todo\n"
+          "5. Statistics\n"
+          "6. Exit\n"
+"••••••••••••••••••••••••••••••••••••••••••••••••••••••••"<<endl;
+	
+	}
 
-
-int mian()
+int main()
 {   
-  
+  help();
 
   return 0;
 }
