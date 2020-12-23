465441a56abf85f8f76c0b8e13f44d63057a34b4
AmalChandru <amaltathva@gmail.com>
2020-12-23 15:28:53 +0530

    updated_help_function_as_per_request

--- main.cpp
+++ main.cpp
@@ -1,16 +1,29 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 using namespace std;
 
 void help(){
-	cout<<"•••••••••••••Welcome to To do List•••••••••••••••\n"
-          ""
-          "1. Add a new to do\n"
-          "2. Show remaining todos\n"
-          "3. Delete a todo\n"
-          "4. Complete a todo\n"
-          "5. Statistics\n"
-          "6. Exit\n"
-"••••••••••••••••••••••••••••••••••••••••••••••••••••••••"<<endl;
+	
+	int width = 80; // a typical assumption of 80 unit width console.
+	
+	string WelcomeLine = "...........WELCOME TO TODO............";
+	
+	int halfWelcomeLineLength = WelcomeLine.length() / 2;
+    int halfOutputWidth = width / 2;
+	
+	int leadingSpaces = halfOutputWidth-halfWelcomeLineLength;
+	int leftMargin = leadingSpaces + 8;
+	
+	std::cout<<std::string(leadingSpaces,' ')<<std::string(11,'.')<<"WELCOME TO TODO"<<std::string(11,'.')<<"\n"
+          "\n"
+          <<std::string(leftMargin,' ')<<"1. Add a new to do\n"
+          <<std::string(leftMargin,' ')<<"2. Show remaining todos\n"
+          <<std::string(leftMargin,' ')<<"3. Delete a todo\n"
+          <<std::string(leftMargin,' ')<<"4. Complete a todo\n"
+          <<std::string(leftMargin,' ')<<"5. Statistics\n"
+          <<std::string(leftMargin,' ')<<"6. Exit\n"
+          <<std::string(leadingSpaces,' ')<<std::string(38,'.')<<endl;
+	
 	
 	}
 
@@ -20,3 +33,4 @@ int main()
 
   return 0;
 }
+
